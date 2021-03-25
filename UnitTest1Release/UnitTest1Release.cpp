4029f5a68eb54f5de74de51e390f63f937a4339b
Nikola Tošić <nikola.tosic98@outlook.com>
2021-03-25 17:38:04 +0100

    generisan projekat za testiranje-release

---
+++ UnitTest1Release/UnitTest1Release.cpp
@@ -0,0 +1,101 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../zadatak_trouglovi/trougao.h"
+#include <limits>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1Release
+{
+	TEST_CLASS(UnitTest1Release)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int kategorija;
+			Assert::AreEqual(12.0, (trougao(3,4,5,kategorija)));
+			Assert::AreEqual(2, kategorija);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			int kategorija;
+			Assert::AreEqual(19.0, (trougao(6, 6, 7, kategorija)));
+			Assert::AreEqual(1, kategorija);
+		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			int kategorija;
+			Assert::AreEqual(19.0, (trougao(6, 6, 7, kategorija)));
+			Assert::AreEqual(1, kategorija);
+		}
+
+		TEST_METHOD(TestMethod4)
+		{
+			int kategorija;
+			Assert::AreEqual(61.5, (trougao(20.5, 20.5, 20.5, kategorija)));
+			Assert::AreEqual(0, kategorija);
+		}
+
+		TEST_METHOD(TestMethod5)
+		{
+			int kategorija;
+			Assert::AreEqual(-1.0, (trougao(30, 70, 250, kategorija)));
+			Assert::AreEqual(-1, kategorija);
+		}
+
+		TEST_METHOD(TestMethod6)
+		{
+			int kategorija;
+			Assert::AreEqual(-1.0, (trougao(-5, 2, 2, kategorija)));
+			Assert::AreEqual(-1, kategorija);
+		}
+
+		TEST_METHOD(TestMethod7)
+		{
+			int kategorija;
+			Assert::AreEqual(-1.0, (trougao(0, 2, 8, kategorija)));
+			Assert::AreEqual(-1, kategorija);
+		}
+
+		TEST_METHOD(TestMethod8)
+		{
+			int kategorija;
+			Assert::AreEqual(-1.0, (trougao(3, -9, 7, kategorija)));
+			Assert::AreEqual(-1, kategorija);
+		}
+
+		TEST_METHOD(TestMethod9)
+		{
+			int kategorija;
+			Assert::AreEqual(-1.0, (trougao(4, 0, 2, kategorija)));
+			Assert::AreEqual(-1, kategorija);
+		}
+
+		TEST_METHOD(TestMethod10)
+		{
+			int kategorija;
+			Assert::AreEqual(-1.0, (trougao(3, 7, -11, kategorija)));
+			Assert::AreEqual(-1, kategorija);
+		}
+
+		TEST_METHOD(TestMethod11)
+		{
+			int kategorija;
+			Assert::AreEqual(-1.0, (trougao(4, 8, 0, kategorija)));
+			Assert::AreEqual(-1, kategorija);
+		}
+
+		TEST_METHOD(TestMethod12)
+		{
+			int kategorija;
+			trougao(10000000000000000.1, 10000000000000000.2, 10000000000000000.3, kategorija);
+			Assert::AreEqual(std::numeric_limits<double>::infinity(), trougao(4, 8, 0, kategorija));
+			//Assert::AreEqual(-1, kategorija);
+		}
+
+
+	};
+}
--- zadatak_trouglovi/trougao.cpp
+++ zadatak_trouglovi/trougao.cpp
@@ -1,50 +1,26 @@
 #include "trougao.h"
 
-bool pitagora(double a, double b, double c)
-{
-	a *= a;
-	b *= b;
-	c *= c;
-	return a == b + c || b == c + a || c == a + b;
-}
-
 double trougao(double a, double b, double c, int& kategorija) {
-	double obim = -1.0;
-	//postoji stranica koja je manja ili jednaka 0 - nedozvoljeno
-	if (a <= 0.0 || b <= 0.0 || c <= 0.0) {
+	if (a <= 0 || b <= 0 || c <= 0) {
 		kategorija = -1;
 		return -1;
 	}
-	//sada znamo da su sve stranice u dozvoljenom opsegu
-	//pitamo se da li može da se kreira trougao
-
-	if (a < b + c && b < a + c && c < a + b) {
-		
-		obim = a + b + c;
-		//ovde znamo da od ovih stranica moze da se kreira trougao
-		//sada ispitujemo tip trougla
-
-		//prvo ispitujemo da li je trougao jednakostranican
-		if (a == b && b == c && c == a) {
+	if (a + b > c && a + c > b && b + c > a) {
+		if (a == b && a == c) {
 			kategorija = 0;
 		}
-		//ako nije jednakostranican pitamo se da li je jednakokraki
-		else if (a == b || b == c || c == a) {
+		else if (a == b || a == c || b == c) {
 			kategorija = 1;
 		}
-		//ako nije jednakokraki pitamo se da li je pravougli
-		else if (pitagora(a, b, c)) {
+		else if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a) {
 			kategorija = 2;
 		}
-		//ovde znamo da od stranica moze da se kreira a da nije ni jedan od prethodnih
 		else {
 			kategorija = 3;
 		}
-		//i na kraju vracamo obim
-		return obim;
+		return a + b + c;
 	}
 	else {
-		//ovde znamo da se od ovih stranica ne moze kreirati trougao
 		kategorija = -1;
 		return -1;
 	}
